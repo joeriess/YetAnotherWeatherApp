2864a8967472385f283c1aaf9de68a69561558a6
Joe Riess <joeriess94@gmail.com>
2017-03-18 12:36:16 -0400

    added table and image view

--- WeatherViewController.swift
+++ WeatherViewController.swift
@@ -20,32 +20,59 @@ final class WeatherViewController: UIViewController {
         return $0
     }(UIView())
     
-    private let timeCityContainer: UIView = {
-        
-        return $0
-    }(UIView())
-    
-    private let dateTimeLabel: UILabel = {
+    private let timeCityLabel: UILabel = {
         
         return $0
     }(UILabel())
     
-    private let cityLabel: UILabel = {
+    private let nowTemperatureLabel: UILabel = {
         
         return $0
     }(UILabel())
     
+    private let headerStackView: UIStackView = {
+
+        return $0
+    }(UIStackView())
+    
+    private let headerBackgroundImageView: UIImageView = {
+        $0.image = #imageLiteral(resourceName: "TGaZIQ")
+        return $0
+    }(UIImageView())
+    
+    private lazy var detailTableView: UITableView = {
+        
+        $0.delegate = self
+        $0.dataSource = self
+        
+        $0.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return $0
+    }(UITableView())
+    
+    // MARK: - Presenter
+    
+    fileprivate var viewModel: WeatherViewModel?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        let fakeViewModel = WeatherViewModel(temperature: (current: "25", high: "30", low: "20"),
+                                             city: "Berlin",
+                                             humidity: "5",
+                                             description: "Cold",
+                                             timeFetched: "MON, 11AM")
+        viewModel = fakeViewModel
         
         view.addSubview(headerView)
+        headerView.addSubview(headerBackgroundImageView)
+        view.addSubview(detailTableView)
         
-        configureView()
+        view.setNeedsUpdateConstraints()
     }
     
-    private func configureView() {
+    
+    override func updateViewConstraints() {
         
         headerView.snp.makeConstraints() { make in
             make.top.equalTo(self.view.snp.topMargin)
@@ -53,5 +80,33 @@ final class WeatherViewController: UIViewController {
             make.right.equalTo(self.view.snp.right)
             make.height.equalTo(self.view.frame.size.height / 2)
         }
+        
+        headerBackgroundImageView.snp.makeConstraints() { make in
+            make.size.equalToSuperview()
+        }
+        
+        detailTableView.snp.makeConstraints() { make in
+            make.size.equalToSuperview()
+            make.top.equalTo(headerView.snp.bottom)
+        }
+        
+        super.updateViewConstraints()
+    }
+    
+}
+
+extension WeatherViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+         return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
     }
 }
